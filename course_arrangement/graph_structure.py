de9f0e5fefce860992982aa5317750f51fd7abc1
YZS <1766891061@qq.com>
2022-12-03 23:36:08 +0800

    all_renew

--- course_arrangement/graph_structure.py
+++ course_arrangement/graph_structure.py
@@ -1,145 +1,154 @@
 import pandas as pd
 
+
 class GraphStack:
     def __init__(self) -> None:
-        self.list=[]
-    
-    def push(self,G):
+        self.list = []
+
+    def push(self, G):
         self.list.append(G)
         return None
-    
+
     def pop(self):
         return self.list.pop()
-    
+
     def isEmpty(self):
         return self.list == []
-    
+
     def size(self):
         return len(self.list)
-    
+
     def top(self):
         return self.list[self.size() - 1]
 
+
 class AdjVexNode:
-    def __init__(self,adjvex=None,next=None) -> None:
-        self.adjvex=adjvex
-        self.next=next
+    def __init__(self, adjvex=None, next=None) -> None:
+        self.adjvex = adjvex
+        self.next = next
+
 
 class VexNode:
     def __init__(self) -> None:
-        self.id=None
-        self.credit=None
-        self.indegree=0
-        self.fst_adjvex=None
+        self.id = None
+        self.credit = None
+        self.indegree = 0
+        self.fst_adjvex = None
+
 
 class Graph:
     def __init__(self) -> None:
-        self.term_num=None
-        self.max_credits=None
-        self.course_num=0
-        self.vex_list=[]
-        self.result=[]
-        self.arrangement=[]
-    
-    def switch(self,id):
-        pos=id.strip("C")
+        self.term_num = None
+        self.max_credits = None
+        self.course_num = 0
+        self.vex_list = []
+        self.result = []
+        self.arrangement = []
+
+    def switch(self, id):
+        pos = id.strip("C")
         return int(pos)
 
     def read_file(self):
-        df_basic=pd.read_json(r"course_arrangement\basic_info.json")
-        df_course=pd.read_csv(r"course_arrangement\course_info.csv",index_col=0)
-        self.term_num=df_basic.iloc[0,0]
-        self.max_credits=df_basic.iloc[0,1]
-        self.course_num=df_basic.iloc[0,2]
-        self.vex_list=[VexNode() for _ in range(self.course_num)]
+        df_basic = pd.read_json(r"course_arrangement\basic_info.json")
+        df_course = pd.read_csv(r"course_arrangement\course_info.csv", index_col=0)
+        self.term_num = df_basic.iloc[0, 0]
+        self.max_credits = df_basic.iloc[0, 1]
+        self.course_num = df_basic.iloc[0, 2]
+        self.vex_list = [VexNode() for _ in range(self.course_num)]
         for i in range(self.course_num):
-            self.vex_list[i].id=df_course.iloc[i,0]
-            self.vex_list[i].credit=df_course.iloc[i,1]
-            pre=df_course.iloc[i,2]
-            if pre==pre:
-                pre_course=pre.split(" ")
+            self.vex_list[i].id = df_course.iloc[i, 0]
+            self.vex_list[i].credit = df_course.iloc[i, 1]
+            pre = df_course.iloc[i, 2]
+            if pre == pre:
+                pre_course = pre.split(" ")
                 for course in pre_course:
-                    node=AdjVexNode(self.switch(course),self.vex_list[i].fst_adjvex)
-                    self.vex_list[i].fst_adjvex=node
+                    node = AdjVexNode(self.switch(course), self.vex_list[i].fst_adjvex)
+                    self.vex_list[i].fst_adjvex = node
         for vex in self.vex_list:
-            tmp=vex.fst_adjvex
+            tmp = vex.fst_adjvex
             while True:
-                if tmp!=None:
-                    self.vex_list[tmp.adjvex-1].indegree+=1
-                    tmp=tmp.next
+                if tmp != None:
+                    self.vex_list[tmp.adjvex - 1].indegree += 1
+                    tmp = tmp.next
                 else:
                     break
         return None
 
     def top_sort(self):
-        flag=0
-        S1=GraphStack()
-        S2=GraphStack()
+        flag = 0
+        S1 = GraphStack()
+        S2 = GraphStack()
         self.read_file()
-        while flag==0:
-            flag=1
+        while flag == 0:
+            flag = 1
             for vex in self.vex_list:
-                if vex.indegree==0:
+                if vex.indegree == 0:
                     S1.push(vex)
-                    vex.indegree-=1
+                    vex.indegree -= 1
                     S2.push(vex.fst_adjvex)
-                    flag=0
-            while S2.isEmpty()==False:
-                p=S2.pop()
-                while p!=None:
-                    self.vex_list[p.adjvex-1].indegree-=1
-                    p=p.next
-        if(S1.size()<self.course_num):
+                    flag = 0
+            while S2.isEmpty() == False:
+                p = S2.pop()
+                while p != None:
+                    self.vex_list[p.adjvex - 1].indegree -= 1
+                    p = p.next
+        if S1.size() < self.course_num:
             print("拓扑排序失败")
         for _ in range(S1.size()):
             self.result.append(S1.pop())
 
         return None
 
-    def arrange(self,choice):
-        if choice==1:
-            if self.course_num%self.term_num<self.term_num/2:
-                max_courses=self.course_num/self.term_num
+    def arrange(self, choice):
+        if choice == 1:
+            if self.course_num % self.term_num < self.term_num / 2:
+                max_courses = self.course_num / self.term_num
             else:
-                max_courses=self.course_num/self.term_num+1
+                max_courses = self.course_num / self.term_num + 1
         else:
-            max_courses=9999
-        arranged=[]
-        i=0
-        while len(arranged)<self.course_num:
-            cur_credits=0
-            cur_arranged=[]
-            while len(cur_arranged)<max_courses and cur_credits<self.max_credits:
-                if len(arranged)==self.course_num:
-                    list=[]
+            max_courses = 9999
+        arranged = []
+        i = 0
+        while len(arranged) < self.course_num:
+            cur_credits = 0
+            cur_arranged = []
+            while len(cur_arranged) < max_courses and cur_credits < self.max_credits:
+                if len(arranged) == self.course_num:
+                    list = []
                     for _ in cur_arranged:
                         list.append(_.id)
                     self.arrangement.append(list)
                     return None
-                p=self.result[i].fst_adjvex
-                flag=False
-                while p!=None:
+                p = self.result[i].fst_adjvex
+                flag = False
+                while p != None:
                     for course in cur_arranged:
-                        if self.switch(course.id)==p.adjvex:
-                            flag=True
+                        if self.switch(course.id) == p.adjvex:
+                            flag = True
                             break
-                    if flag==True:
+                    if flag == True:
                         break
-                    p=p.next
-                if flag==False:
-                    if(len(cur_arranged)>=max_courses or cur_credits+self.result[i].credit>self.max_credits):
+                    p = p.next
+                if flag == False:
+                    if (
+                        len(cur_arranged) >= max_courses
+                        or cur_credits + self.result[i].credit > self.max_credits
+                    ):
                         break
                     cur_arranged.append(self.result[i])
                     arranged.append(self.result[i])
-                    cur_credits+=self.result[i].credit
-                    i+=1                    
+                    cur_credits += self.result[i].credit
+                    i += 1
                 else:
                     break
-            list=[]
+            list = []
             for _ in cur_arranged:
                 list.append(_.id)
             self.arrangement.append(list)
         return None
+
+
 """
 C1
 C9
@@ -154,4 +163,3 @@ C12
 C8
 C7
 """
-
--- course_arrangement/input.py
+++ course_arrangement/input.py
@@ -2,116 +2,136 @@ import pandas as pd
 from graph_structure import *
 import os
 
+
 def menu():
-    os.system('cls')
+    os.system("cls")
     print("		     	               欢迎使用课程编排系统	                          \n")
-    print("____________________________________________________________________________________________________\n")
+    print(
+        "____________________________________________________________________________________________________\n"
+    )
     print("			                1. 输入课程信息        			                      \n")
-    print("____________________________________________________________________________________________________\n")
+    print(
+        "____________________________________________________________________________________________________\n"
+    )
     print("	 		                2. 查看上次输入课程信息                		                  \n")
-    print("____________________________________________________________________________________________________\n")                 
-    print("        		                3. 根据上次输入信息均匀安排课程                   		                  \n")
-    print("____________________________________________________________________________________________________\n")                              
-    print("        		                4. 根据上次输入信息尽快安排课程           			                      \n")
-    print("____________________________________________________________________________________________________\n")
+    print(
+        "____________________________________________________________________________________________________\n"
+    )
+    print(
+        "        		                3. 根据上次输入信息均匀安排课程                   		                  \n"
+    )
+    print(
+        "____________________________________________________________________________________________________\n"
+    )
+    print(
+        "        		                4. 根据上次输入信息尽快安排课程           			                      \n"
+    )
+    print(
+        "____________________________________________________________________________________________________\n"
+    )
     print("        		                5. 关闭程序               			                      \n")
-    print("____________________________________________________________________________________________________\n")
+    print(
+        "____________________________________________________________________________________________________\n"
+    )
     print("\n\t ")
-    key=input("请选择您需要的功能：")
-    key=int(key)
-    if key==1:
-        os.system('cls')
+    key = input("请选择您需要的功能：")
+    key = int(key)
+    if key == 1:
+        os.system("cls")
         input_data()
-        print('\n\n')
-        key=input('按任意键返回主菜单')
-    elif key==2:
-        os.system('cls')
+        print("\n\n")
+        key = input("按任意键返回主菜单")
+    elif key == 2:
+        os.system("cls")
         read_data()
-        print('\n\n')
-        key=input('按任意键返回主菜单')
-    elif key==3:
-        G=Graph()
+        print("\n\n")
+        key = input("按任意键返回主菜单")
+    elif key == 3:
+        G = Graph()
         G.read_file()
         G.top_sort()
         G.arrange(1)
-        os.system('cls')
+        os.system("cls")
         print("课程安排如下：")
         for i in range(len(G.arrangement)):
-            print(f"    第{i+1}学期应上课程为：",end='')
-            flag=False          
+            print(f"    第{i+1}学期应上课程为：", end="")
+            flag = False
             for j in G.arrangement[i]:
-                if flag==False:
-                    flag=True
-                    print(j,end='')
+                if flag == False:
+                    flag = True
+                    print(j, end="")
                 else:
-                    print(',',j,end='')
-            print('')
-        print('\n\n')
-        key=input('按任意键返回主菜单')
-    elif key==4:
-        G=Graph()
+                    print(",", j, end="")
+            print("")
+        print("\n\n")
+        key = input("按任意键返回主菜单")
+    elif key == 4:
+        G = Graph()
         G.read_file()
         G.top_sort()
         G.arrange(2)
-        os.system('cls')
+        os.system("cls")
         print("课程安排如下：")
         for i in range(len(G.arrangement)):
-            print(f"    第{i+1}学期应上课程为：",end='')
-            flag=False          
+            print(f"    第{i+1}学期应上课程为：", end="")
+            flag = False
             for j in G.arrangement[i]:
-                if flag==False:
-                    flag=True
-                    print(j,end='')
+                if flag == False:
+                    flag = True
+                    print(j, end="")
                 else:
-                    print(',',j,end='')
-            print('')
-        print('\n\n')
-        key=input('按任意键返回主菜单')
-    elif key==5:
-        os.system('cls')
+                    print(",", j, end="")
+            print("")
+        print("\n\n")
+        key = input("按任意键返回主菜单")
+    elif key == 5:
+        os.system("cls")
         return None
     else:
-        os.system('cls')
-        key=input('指令无效，按任意键返回主菜单')
+        os.system("cls")
+        key = input("指令无效，按任意键返回主菜单")
     menu()
 
+
 def input_data():
-    basic_info={}
-    basic_info["term num"]=int(input("请输入学期总数："))
-    basic_info["credits limit"]=int(input("请输入每学期学分上限："))
-    basic_info["course num"]=int(input("请输入课程总数："))
-    df_info=pd.DataFrame(basic_info,index=[0])
-    df_info.to_json(path_or_buf=r"course_arrangement\basic_info.json",orient="records")
-    course_info={"id":[],"credits":[],"previous":[]}
+    basic_info = {}
+    basic_info["term num"] = int(input("请输入学期总数："))
+    basic_info["credits limit"] = int(input("请输入每学期学分上限："))
+    basic_info["course num"] = int(input("请输入课程总数："))
+    df_info = pd.DataFrame(basic_info, index=[0])
+    df_info.to_json(path_or_buf=r"course_arrangement\basic_info.json", orient="records")
+    course_info = {"id": [], "credits": [], "previous": []}
     print("请依次输入课程ID，课程学分，需先修课程ID（结束时请输入-1）:")
-    course=""
-    couse_list=[]
+    course = ""
+    couse_list = []
     while True:
-        course_info={}
-        course=input()
-        if(course=="-1"):
+        course_info = {}
+        course = input()
+        if course == "-1":
             break
-        info_list=course.split(",")
-        if(len(info_list)==3):
-            course_info["id"]=info_list[0]
-            course_info["credits"]=info_list[1]
-            course_info["previous"]=info_list[2]
+        info_list = course.split(",")
+        if len(info_list) == 3:
+            course_info["id"] = info_list[0]
+            course_info["credits"] = info_list[1]
+            course_info["previous"] = info_list[2]
         else:
-            course_info["id"]=info_list[0]
-            course_info["credits"]=info_list[1]
-            course_info["previous"]="NULL"
+            course_info["id"] = info_list[0]
+            course_info["credits"] = info_list[1]
+            course_info["previous"] = "NULL"
         couse_list.append(course_info.copy())
-    df_course=pd.DataFrame(couse_list)
+    df_course = pd.DataFrame(couse_list)
     df_course.to_csv(path_or_buf=r"course_arrangement\course_info.csv")
     return None
 
+
 def read_data():
-    df=pd.read_csv(r'course_arrangement\course_info.csv')
-    df.drop(columns=df.columns[0], axis=1,inplace=True)
-    df.fillna("无",inplace=True)
+    df = pd.read_csv(r"course_arrangement\course_info.csv")
+    df.drop(columns=df.columns[0], axis=1, inplace=True)
+    df.fillna("无", inplace=True)
     print("课程ID    学分    先修课程")
     for i in range(len(df.index)):
-        print(f'{df.iloc[i,0]}         {df.iloc[i,1]}         {df.iloc[i,2]}')
+        print(f"{df.iloc[i,0]}         {df.iloc[i,1]}         {df.iloc[i,2]}")
 
-if __name__=='__main__':    
+
+if __name__ == "__main__":
     menu()
